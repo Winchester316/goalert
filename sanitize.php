687dc5e50bb93c679e01433d35d98d2058543d24
Winchester316 <yer.nosyaj@gmail.com>
2022-12-16 19:20:20 +0800

    Delete sanitize.php

--- sanitize.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Sanitize form input
-function sanitize($input)
-{
-    $input = htmlspecialchars($input);
-    $input = stripslashes($input);
-    $input = trim($input);
-    return $input;
-}
